1bc878482ec1838a74ce8952d943afd47963bdf6
Ammal77 <p25santosh@gmail.com>
2016-10-16 23:12:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function createTemplate(data)
 }
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui/index.html'));
 });
 app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname, 'article-one.html'));
