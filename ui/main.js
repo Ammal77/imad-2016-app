8aa10a1957059884303010579587da8b512a1e1d
Ammal77 <p25santosh@gmail.com>
2016-10-24 19:12:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 
-var ele=document. getElementById('img');
+var ele=document.getElementById('img');
 function moveRight(){
- marginLeft =marginLeft + 10;
+ marginLeft =marginLeft + 1;
  ele.style.marginLeft = marginLeft + 'px';
 }
 ele.onclick=function(){
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,5);
 };
