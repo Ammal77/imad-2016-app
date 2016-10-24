b69d2a1dcf61be5830a4cb44d894e18e7817f2b6
Ammal77 <p25santosh@gmail.com>
2016-10-24 19:06:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 
+var ele=document.getElemenyById('img');
+function moveRight(){
+ marginLeft =marginLeft + 10;
+ ele.style.marginLeft = marginLeft + 'px';
+}
+ele.onclick=function(){
+    var interval = setInterval(moveRight,50);
+};
