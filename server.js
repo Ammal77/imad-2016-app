c724301bf0b1451ed790329cc340a796eb7d3eb8
Ammal77 <p25santosh@gmail.com>
2016-10-24 20:32:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function createTemplate(data){
              ${date}
              </div>
              <div>
-             $(content)
+              ${content}
              </div>
         </div>
     </body>
